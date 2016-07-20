d1ef8acedfc03803160159c80231ffc026e7cceb
s-friscia <salvatore.friscia@fairmat.com>
2016-07-20 16:28:40 +0200

    Create KidVersion.cs

---
+++ client-sample/c-sharp/Model/KidVersion.cs
@@ -0,0 +1,45 @@
+///
+// This file contains utility classes to access easily OpenKID repositories
+// in order to extract information from them
+//
+// LICENCE:
+// Copyright (c) 2016 Fairmat SRL.
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// <copyright>Copyright (c) 2016 Fairmat SRL (http://www.fairmat.com)</copyright>
+// <license>MIT.</license>
+// <version>0.1</version>
+///
+///
+// An implementation of several utils to access OpenKID repositories.
+//
+// <copyright>Copyright (c) 2016 Fairmat SRL (http://www.fairmat.com)</copyright>
+// <license>MIT.</license>
+// <version>0.1</version>
+///
+
+namespace openKid.Model
+{
+    public class KidVersion
+    {
+        public string Language { get; set; }
+        public string Url { get; set; }
+    }
+}
